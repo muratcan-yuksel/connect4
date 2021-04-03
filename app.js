b7be7958a39356ed5bf3d155255249b1a6be5e85
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-03 07:03:09 +0300

    added the grid items

---
+++ app.js
@@ -0,0 +1,17 @@
+const container=document.querySelector('.container');
+// create 49 divs of which the last 7 are the base grids, i.e. taken
+function createBoard() {
+  for (let i =0; i<42; i++) {
+    const grid= document.createElement('div');
+    console.log(grid[i]);
+    container.appendChild(grid);
+  }
+  for (let y= 0; y<7; y++) {
+    const taken= document.createElement('div');
+    console.log(taken);
+    taken.setAttribute('class', 'taken');
+    container.appendChild(taken);
+  }
+}
+
+createBoard();
