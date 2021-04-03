ec1a93698cca40648534e9727539f94f24a35b0b
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-03 07:38:45 +0300

    players can put their marks now

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container=document.querySelector('.container');
+
 // create 49 divs of which the last 7 are the base grids, i.e. taken
 function createBoard() {
   for (let i =0; i<42; i++) {
@@ -14,12 +15,27 @@ function createBoard() {
   }
 }
 
-
 createBoard();
-const currentPlayer= document.getElementById('current-player');
+
+let displayCurrentPlayer= document.getElementById('current-player');
 const squares= document.querySelectorAll('.container div');
+const result= document.getElementById("result");
+let currentPlayer=1;
+
 for (let i=0; i<squares.length; i++) {
   squares[i].onclick=()=>{
-
+    if(squares[i + 7].classList.contains("taken") && !squares[i].classList.contains("taken")){
+        if(currentPlayer == 1){
+            squares[i].classList.add("taken");
+            squares[i].classList.add("player-one");
+            currentPlayer = 2;
+            displayCurrentPlayer.innerHTML=currentPlayer;
+        }else if(currentPlayer == 2){
+            squares[i].classList.add("taken");
+            squares[i].classList.add("player-two");
+            currentPlayer = 1;
+            displayCurrentPlayer.innerHTML=currentPlayer;
+        }   
+     }
   };
 }
